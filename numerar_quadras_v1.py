8b4dbe487a1f4bbe90a82b14d3cb36d3a0fed934
Gustavo Salles <87676513+salles97@users.noreply.github.com>
2024-01-24 09:54:44 -0300

    feat: :memo: Add readme

--- numerar_quadras_v1.py
+++ numerar_quadras_v1.py
@@ -2,9 +2,7 @@ import geopandas as gpd
 import psycopg2
 from shapely import wkt
 from shapely.geometry import Point, box
-
 import matplotlib.pyplot as plt
-
 
 # Conecta ao banco
 conn = psycopg2.connect(database="Itanhandu_stagging", user="postgres", password="654741", host="localhost", port="5432")
@@ -17,38 +15,16 @@ cur.execute('SELECT ST_AsText(geom) as geometry FROM public.quadras_teste_algori
 gdf = gpd.GeoDataFrame(cur.fetchall(), columns=['geometry'])
 # Define a coluna 'geometry' como a coluna de geometria ativa
 gdf['geometry'] = gdf['geometry'].apply(wkt.loads)
-print(gdf)
 
 gdf = gdf.set_geometry('geometry')
-
 gdf.set_crs(epsg=31983) 
-
-# Converte a representação textual para objetos Shapely
 
 # Cria o Bounding Box
 bbox = gdf.total_bounds
-bbox_polygon  = box(*bbox)
-
-# Seleciona a coordenada mais ao sul e ao oeste
-coord_inicial = bbox.min()  # Atribui o valor mínimo a uma variável
+bbox_polygon = box(*bbox)
 
 # Seleciona a coordenada mais ao sul e ao oeste
 coord_inicial = Point(bbox[0], bbox[1])  # Cria um objeto Point
-
-print(bbox)
-
-# Plotar o GeoDataFrame
-fig, ax = plt.subplots()
-gdf.plot(ax=ax, color='lightgray', edgecolor='black')
-
-# Plotar o BoundingBox
-gpd.GeoSeries([bbox_polygon]).plot(ax=ax, facecolor='none', edgecolor='red', linestyle='--')
-
-# Adicionar título
-plt.title('GeoDataFrame com BoundingBox')
-
-# Mostrar o gráfico
-plt.show()
 
 # Inicializa a coluna cod com 0
 gdf['cod'] = 0
@@ -56,37 +32,23 @@ gdf['cod'] = 0
 cod = 1
 
 def encontrar_vizinhos(gdf, ponto_atual, cod):
-    # Adicione mensagens de depuração
-    print(f'Entrando no loop com ponto_atual: {ponto_atual}, cod: {cod}')
     ponto_inicial = Point(ponto_atual)
     
     # Verifica se há polígonos não visitados
     while not gdf.loc[gdf.cod == 0].empty: 
-        print(f'Dentro do loop com ponto_atual: {ponto_atual}, cod: {cod}')
         # Seleciona o polígono mais próximo da coordenada atual
         poligonos_nao_visitados = gdf.loc[gdf.cod == 0]
-        
-        print(len(poligonos_nao_visitados))
-        # Converte a coordenada atual em um GeoSeries
         ponto_atual = Point(ponto_atual)
-        
         poligono_proximo_idx = poligonos_nao_visitados.geometry.distance(ponto_atual).idxmin()
 
-        # Adicione mensagens de depuração
-        print(f'poligono_proximo_idx: {poligono_proximo_idx}')
-        
         # Verifica se o polígono mais próximo está dentro do intervalo desejado
         if poligonos_nao_visitados.loc[poligono_proximo_idx, 'geometry'].distance(ponto_atual) <= 1000:
             # Cria o Bounding Box do polígono selecionado
             bbox_proximo = gdf.loc[poligono_proximo_idx, 'geometry'].bounds
             
-            # Adicione mensagens de depuração
-            print(f'Atualizando bbox_proximo: {bbox_proximo}')
-            
             # Verifica se há vizinhos dentro do intervalo no novo Bounding Box
             vizinhos_proximos = gdf[gdf.geometry.distance(gdf.loc[poligono_proximo_idx, 'geometry']) <= 100]
 
-            print(len(vizinhos_proximos))
             # Verifica se há vizinhos próximos
             if not vizinhos_proximos.empty:
                 # Atualiza a coluna cod do polígono selecionado
@@ -96,8 +58,7 @@ def encontrar_vizinhos(gdf, ponto_atual, cod):
                 ponto_atual = Point(bbox_proximo[2], bbox_proximo[1])
                 
             else:
-        
-                # Adicionado valor de cod ao poligono_proximo_idx
+                # Adiciona valor de cod ao poligono_proximo_idx
                 gdf.loc[poligono_proximo_idx, 'cod'] = cod
                 cod += 1
                 ponto_atual = ponto_inicial
@@ -110,11 +71,30 @@ def encontrar_vizinhos(gdf, ponto_atual, cod):
     # Exportar GeoDataFrame para Shapefile
     gdf.to_file('./teste_quadras_numeradas.shp', driver='ESRI Shapefile')
 
-                
+# Plotar o GeoDataFrame
+fig, ax = plt.subplots()
+gdf.plot(ax=ax, color='lightgray', edgecolor='black')
+
+# Plotar o BoundingBox
+gpd.GeoSeries([bbox_polygon]).plot(ax=ax, facecolor='none', edgecolor='red', linestyle='--')
+
+# Plotar o ponto inicial
+gpd.GeoSeries([coord_inicial]).plot(ax=ax, color='blue', markersize=50, label='Ponto Inicial')
+
 # Chama a função inicialmente
 encontrar_vizinhos(gdf, coord_inicial, cod)
 
-print(gdf)
-print('acabo')
+# Plotar os polígonos com os códigos finais
+gdf.plot(ax=ax, column='cod', legend=True, cmap='viridis', legend_kwds={'label': "Códigos"})
+
+# Adicionar título
+plt.title('GeoDataFrame com BoundingBox e Códigos')
+
+# Adicionar legenda
+plt.legend()
+
+# Mostrar o gráfico
+plt.show()
+
 # Feche a conexão
 conn.close()
---
+++ readme.md
@@ -0,0 +1,63 @@
+<h1 align="center">Algoritmo de atribuição de Inscrição Municipal</h1>
+
+
+ <h4 align="center"> 
+	🚧  Algoritmo 🚀 Em construção...  🚧
+</h4>
+
+
+## Sobre
+
+ 
+ 
+## Sumário 
+<!--ts-->
+   * [Sobre](#sobre)
+   * [Sumário](#sumário)
+   * [Features](#features)
+   * [Instalação](#instalação) 
+      * [Pre Requisitos](#pré-requisitos) 
+      * [Como Rodar](#como-rodar)   
+   * [Contribuição](#contribuição)
+   * [Licença](#licença)
+<!--te-->
+ 
+ 
+## Features
+
+- [x] Renumerar as Quadras
+- [ ] Renumerar os Lotes
+- [ ] Renumerar os imóveis
+
+
+## Instalação
+### Pré-requisitos 
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com), [Python](https://www.python.org/). 
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+### Como Rodar
+```
+
+# Clone este repositório
+$  git clone <https://github.com/salles97/Algoritmo-de-Inscricao> 
+
+# Acesse a pasta do projeto
+$ cd Algoritmo de Inscricao
+ 
+# Instale as dependências Python
+$ pip install -r requirements.txt
+
+# Execute o script Python
+python script.py numerar_quadras
+```
+ 
+ 
+## Contribuição
+ 
+Faça um fork do projeto
+Crie uma branch para sua modificação (git checkout -b feature/nova-feature)
+Faça commit das suas alterações (git commit -am 'Adiciona nova feature')
+Faça push para a branch (git push origin feature/nova-feature)
+Abra um Pull Request
+ 
